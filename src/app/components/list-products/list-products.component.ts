df62572aada3aa311d30ae79713be96db42a3ec4
Paul <paul.souille85@gmail.com>
2020-02-06 11:14:35 +0100

    add products

--- src/app/components/list-products/list-products.component.ts
+++ src/app/components/list-products/list-products.component.ts
@@ -31,53 +31,53 @@ export class ListProductsComponent implements OnInit {
       name: 'vélo',
       description:'Un vélo permet de se déplacer facilement d\'un point A a un point B pas trop loins'
   },      {id:'1',
-  price:'15',
+  price:'1000',
   date: new Date(), 
   image:'',
-  name: 'vélo',
-  description:'Un vélo permet de se déplacer facilement d\'un point A a un point B pas trop loins'
+  name: 'Ordinateur',
+  description:'Ordinateur pour gamer'
+},      {id:'1',
+price:'150',
+date: new Date(), 
+image:'',
+name: 'Airpods',
+description:'Pour écouter la musique comme un PRO'
+},      {id:'1',
+price:'2',
+date: new Date(), 
+image:'',
+name: 'Pain au chocolat',
+description:'Petit plaisir coupable du matin'
+},      {id:'1',
+price:'800',
+date: new Date(), 
+image:'',
+name: 'Mac Book',
+description:'Ordinateur pour les petits cerveaux'
+},      {id:'1',
+price:'1',
+date: new Date(), 
+image:'',
+name: 'Pomme',
+description:'Pour manger sain !'
 },      {id:'1',
 price:'15',
 date: new Date(), 
 image:'',
-name: 'vélo',
-description:'Un vélo permet de se déplacer facilement d\'un point A a un point B pas trop loins'
+name: 'Tacos',
+description:'Pour manger !'
 },      {id:'1',
-price:'15',
+price:'2000',
 date: new Date(), 
 image:'',
-name: 'vélo',
-description:'Un vélo permet de se déplacer facilement d\'un point A a un point B pas trop loins'
+name: 'Voiture',
+description:'Pour se déplacer en tout sécurité'
 },      {id:'1',
-price:'15',
+price:'80',
 date: new Date(), 
 image:'',
-name: 'vélo',
-description:'Un vélo permet de se déplacer facilement d\'un point A a un point B pas trop loins'
-},      {id:'1',
-price:'15',
-date: new Date(), 
-image:'',
-name: 'vélo',
-description:'Un vélo permet de se déplacer facilement d\'un point A a un point B pas trop loins'
-},      {id:'1',
-price:'15',
-date: new Date(), 
-image:'',
-name: 'vélo',
-description:'Un vélo permet de se déplacer facilement d\'un point A a un point B pas trop loins'
-},      {id:'1',
-price:'15',
-date: new Date(), 
-image:'',
-name: 'vélo',
-description:'Un vélo permet de se déplacer facilement d\'un point A a un point B pas trop loins'
-},      {id:'1',
-price:'15',
-date: new Date(), 
-image:'',
-name: 'vélo',
-description:'Un vélo permet de se déplacer facilement d\'un point A a un point B pas trop loins'
+name: 'Chaise',
+description:'Pour pouvoir s\'asseoir'
 }
     ]
     return products
